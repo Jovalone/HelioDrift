9e4b02dd93ac0e7590bec80b50065facb358051e
kimslor <68843643+kimslor@users.noreply.github.com>
2022-03-19 10:38:06 +1300

    Update MapCamera.cs
    I added something.

--- Assets/CameraControl/MapCamera.cs
+++ Assets/CameraControl/MapCamera.cs
@@ -7,7 +7,7 @@ public class MapCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        lol = 0;
     }
 
     // Update is called once per frame
